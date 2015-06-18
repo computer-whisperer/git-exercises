c995e50a9fe0af6f652451859a04532fa7ba7f62
Christian Balcom <robot.inventor@gmail.com>
2015-06-18 09:26:26 -0400

    Added a semi-malicious evil laugh to mystery_python_file.py

--- mystery_python_file.py
+++ mystery_python_file.py
@@ -1,6 +1,11 @@
-__author__ = 'Christian'
+import time
 
 print("This is a riddle, wrapped in a mystery!")
 print("This is new.")
 print("This is even newer!")
 print("Taylor has made it even newer than before!")
+
+# Evil chuckle!
+while True:
+    print("hahaha!")
+    time.sleep(1)
